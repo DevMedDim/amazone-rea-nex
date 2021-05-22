b18714bd3613e8e726eb1a653c131c1e93434602
DevMedDim <mrakdimdev@gmail.com>
2021-05-22 17:55:19 +0100

    fake store api, main content, fetching products, reender and product list layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,37 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({products}) {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon</title>
       </Head>
 
+
       {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
-      
-        {/* HEADER */}
-        <Header   />
+
+      {/* HEADER */}
+      <Header />
+      <main className="max-w-screen-2xl mx-auto" >
+        {/* Banner */}
+        <Banner />
+        {/* ProductFeed */}
+        <ProductFeed products={products} />
+      </main>
       {/* ---- ---- */}
     </div>
   );
+}
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      products,
+    },
+  };
 }
